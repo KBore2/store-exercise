2a80e5e0cd6c38810d1fffaae194a3014b814c0f
KBore2 <99807846+KBore2@users.noreply.github.com>
2023-06-22 20:58:12 +0200

    feat: :sparkles: products-card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ProductsContainerComponent } from './products-container/products-container.component';
+import { ProductCardComponent } from './products-container/product-card/product-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ProductsContainerComponent,
+    ProductCardComponent
   ],
   imports: [
     BrowserModule,
