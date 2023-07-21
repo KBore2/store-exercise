d6325d08893b75a5106d1b0001d28e8d1bf436b2
KBore2 <99807846+KBore2@users.noreply.github.com>
2023-07-21 15:13:45 +0200

    Revert "Develop"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductPageComponent } from './components/body/product-page/product-page.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { CheckoutComponent } from './components/checkout/checkout.component';
+import { ProductsContainerComponent } from './components/body/products-container/products-container.component';
+import { CartContainerComponent } from './components/body/cart-container/cart-container.component';
+import { checkoutGuard } from './checkout.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'sign-up', component: SignUpComponent },
+  {
+    path: 'checkout',
+    component: CheckoutComponent,
+    canActivate: [checkoutGuard],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
