468e4b1bc1849b284b96f5108fb593bcfd767a1c
KBore2 <99807846+KBore2@users.noreply.github.com>
2023-07-21 15:15:09 +0200

    Revert "Revert 5 feature/checkout forms"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductsContainerComponent } from './components/products-container/products-container.component';
+import { CartContainerComponent } from './components/cart-container/cart-container.component';
+import { ProductPageComponent } from './components/product-page/product-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ProductsContainerComponent },
+  { path: 'cart', component: CartContainerComponent },
+  { path: 'details/:id', component: ProductPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
